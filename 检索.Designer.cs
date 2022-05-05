d7a6ef2477c1db5e9043015ac7fe2170f63a0b9a
MJ C <2652127675@qq.com>
2022-05-06 01:31:29 +0800

    添加 README.md

---
+++ 检索.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace 搜索
+{
+    partial class 检索
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.加载 = new System.Windows.Forms.Button();
+            this.结果 = new System.Windows.Forms.TextBox();
+            this.作者 = new System.Windows.Forms.Label();
+            this.搜索 = new System.Windows.Forms.Label();
+            this.书名 = new System.Windows.Forms.Label();
+            this.作者框 = new System.Windows.Forms.TextBox();
+            this.搜索按钮 = new System.Windows.Forms.Button();
+            this.书名框 = new System.Windows.Forms.TextBox();
+            this.生成 = new System.ComponentModel.BackgroundWorker();
+            this.文件 = new System.Windows.Forms.OpenFileDialog();
+            this.检索书名 = new System.ComponentModel.BackgroundWorker();
+            this.检索作者 = new System.ComponentModel.BackgroundWorker();
+            this.输出 = new System.ComponentModel.BackgroundWorker();
+            this.取消 = new System.Windows.Forms.Button();
+            this.进度条 = new System.Windows.Forms.ProgressBar();
+            this.索引框 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // 加载
+            // 
+            this.加载.Location = new System.Drawing.Point(732, 47);
+            this.加载.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.加载.Name = "加载";
+            this.加载.Size = new System.Drawing.Size(85, 23);
+            this.加载.TabIndex = 15;
+            this.加载.Text = "加载数据源";
+            this.加载.UseVisualStyleBackColor = true;
+            this.加载.Click += new System.EventHandler(this.加载_Click);
+            // 
+            // 结果
+            // 
+            this.结果.AcceptsReturn = true;
+            this.结果.AcceptsTab = true;
+            this.结果.Location = new System.Drawing.Point(100, 77);
+            this.结果.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.结果.MaxLength = 2147483647;
+            this.结果.Multiline = true;
+            this.结果.Name = "结果";
+            this.结果.ReadOnly = true;
+            this.结果.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.结果.Size = new System.Drawing.Size(717, 501);
+            this.结果.TabIndex = 14;
+            // 
+            // 作者
+            // 
+            this.作者.AutoSize = true;
+            this.作者.Location = new System.Drawing.Point(397, 20);
+            this.作者.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.作者.Name = "作者";
+            this.作者.Size = new System.Drawing.Size(32, 17);
+            this.作者.TabIndex = 13;
+            this.作者.Text = "作者";
+            // 
+            // 搜索
+            // 
+            this.搜索.AutoSize = true;
+            this.搜索.Location = new System.Drawing.Point(51, 50);
+            this.搜索.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.搜索.Name = "搜索";
+            this.搜索.Size = new System.Drawing.Size(44, 17);
+            this.搜索.TabIndex = 12;
+            this.搜索.Text = "搜索框";
+            // 
+            // 书名
+            // 
+            this.书名.AutoSize = true;
+            this.书名.Location = new System.Drawing.Point(98, 20);
+            this.书名.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.书名.Name = "书名";
+            this.书名.Size = new System.Drawing.Size(32, 17);
+            this.书名.TabIndex = 11;
+            this.书名.Text = "书名";
+            // 
+            // 作者框
+            // 
+            this.作者框.Location = new System.Drawing.Point(399, 47);
+            this.作者框.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.作者框.Name = "作者框";
+            this.作者框.Size = new System.Drawing.Size(165, 23);
+            this.作者框.TabIndex = 10;
+            this.作者框.TextChanged += new System.EventHandler(this.作者框_TextChanged);
+            // 
+            // 搜索按钮
+            // 
+            this.搜索按钮.Location = new System.Drawing.Point(569, 47);
+            this.搜索按钮.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.搜索按钮.Name = "搜索按钮";
+            this.搜索按钮.Size = new System.Drawing.Size(75, 23);
+            this.搜索按钮.TabIndex = 9;
+            this.搜索按钮.Text = "搜索";
+            this.搜索按钮.UseVisualStyleBackColor = true;
+            this.搜索按钮.Click += new System.EventHandler(this.搜索按钮_Click);
+            // 
+            // 书名框
+            // 
+            this.书名框.Location = new System.Drawing.Point(100, 47);
+            this.书名框.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.书名框.Name = "书名框";
+            this.书名框.Size = new System.Drawing.Size(294, 23);
+            this.书名框.TabIndex = 8;
+            this.书名框.TextChanged += new System.EventHandler(this.书名框_TextChanged);
+            // 
+            // 生成
+            // 
+            this.生成.WorkerSupportsCancellation = true;
+            this.生成.DoWork += new System.ComponentModel.DoWorkEventHandler(this.生成_DoWork);
+            // 
+            // 文件
+            // 
+            this.文件.FileName = "文件";
+            // 
+            // 检索书名
+            // 
+            this.检索书名.WorkerSupportsCancellation = true;
+            this.检索书名.DoWork += new System.ComponentModel.DoWorkEventHandler(this.检索书名_DoWork);
+            // 
+            // 检索作者
+            // 
+            this.检索作者.WorkerSupportsCancellation = true;
+            this.检索作者.DoWork += new System.ComponentModel.DoWorkEventHandler(this.检索作者_DoWork);
+            // 
+            // 输出
+            // 
+            this.输出.WorkerSupportsCancellation = true;
+            this.输出.DoWork += new System.ComponentModel.DoWorkEventHandler(this.输出_DoWork);
+            // 
+            // 取消
+            // 
+            this.取消.Location = new System.Drawing.Point(649, 47);
+            this.取消.Margin = new System.Windows.Forms.Padding(5, 15, 0, 0);
+            this.取消.Name = "取消";
+            this.取消.Size = new System.Drawing.Size(75, 23);
+            this.取消.TabIndex = 16;
+            this.取消.Text = "取消";
+            this.取消.UseVisualStyleBackColor = true;
+            this.取消.Click += new System.EventHandler(this.取消_Click);
+            // 
+            // 进度条
+            // 
+            this.进度条.Location = new System.Drawing.Point(12, 581);
+            this.进度条.Name = "进度条";
+            this.进度条.Size = new System.Drawing.Size(805, 23);
+            this.进度条.TabIndex = 17;
+            // 
+            // 索引框
+            // 
+            this.索引框.Location = new System.Drawing.Point(12, 77);
+            this.索引框.Multiline = true;
+            this.索引框.Name = "索引框";
+            this.索引框.ReadOnly = true;
+            this.索引框.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.索引框.Size = new System.Drawing.Size(83, 498);
+            this.索引框.TabIndex = 18;
+            // 
+            // 检索
+            // 
+            this.ClientSize = new System.Drawing.Size(840, 615);
+            this.Controls.Add(this.索引框);
+            this.Controls.Add(this.进度条);
+            this.Controls.Add(this.取消);
+            this.Controls.Add(this.加载);
+            this.Controls.Add(this.结果);
+            this.Controls.Add(this.作者);
+            this.Controls.Add(this.搜索);
+            this.Controls.Add(this.书名);
+            this.Controls.Add(this.作者框);
+            this.Controls.Add(this.搜索按钮);
+            this.Controls.Add(this.书名框);
+            this.MaximizeBox = false;
+            this.Name = "检索";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button 加载;
+        private System.Windows.Forms.TextBox 结果;
+        private System.Windows.Forms.Label 作者;
+        private System.Windows.Forms.Label 搜索;
+        private System.Windows.Forms.Label 书名;
+        private System.Windows.Forms.TextBox 作者框;
+        private System.Windows.Forms.Button 搜索按钮;
+        private System.Windows.Forms.TextBox 书名框;
+        private System.ComponentModel.BackgroundWorker 生成;
+        private System.Windows.Forms.OpenFileDialog 文件;
+        private System.ComponentModel.BackgroundWorker 检索书名;
+        private System.ComponentModel.BackgroundWorker 检索作者;
+        private System.ComponentModel.BackgroundWorker 输出;
+        private Button 取消;
+        private ProgressBar 进度条;
+        private TextBox 索引框;
+    }
+}
---
+++ 检索.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+
+namespace 搜索
+{
+    public partial class 检索 : Form
+    {
+        string file0 = ".\\test.txt";
+        int[] i1 = { -1 };
+        private CancellationTokenSource 搜索_stop;
+
+        public 检索()
+        {
+            InitializeComponent();
+            生成 = new BackgroundWorker();
+
+        }
+
+        private void 搜索按钮_Click(object sender, EventArgs e)
+        {
+            搜索_stop = new CancellationTokenSource();
+            ThreadPool.QueueUserWorkItem(state => Out(搜索_stop.Token));
+
+        }
+
+        private void 加载_Click(object sender, EventArgs e)
+        {
+            if (文件.ShowDialog() == DialogResult.OK)
+            {
+                file0 = 文件.FileName;
+                System.Diagnostics.Trace.WriteLine(file0);
+                生成.DoWork += new DoWorkEventHandler(生成_DoWork);
+                生成.RunWorkerAsync(this);
+
+            }
+            if (生成.IsBusy)
+            {
+                System.Diagnostics.Debug.WriteLine("正在生成");
+            }
+            System.Diagnostics.Debug.WriteLine("???");
+        }
+
+        private void 生成_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ArrayList arr = new();
+            arr.Add(1);
+            System.Diagnostics.Debug.WriteLine("!!!");
+
+            string file1Path = ".\\数据索引\\";
+            //创建目录
+            System.IO.Directory.CreateDirectory(file1Path);
+            //打开流
+            System.IO.StreamReader file = new(file0, Encoding.UTF8, true);
+            //遍历所有行
+            for (int n = 0; file.Peek() != -1; n++)
+            {
+                //把每一行塞进数组
+                char[] chars = file.ReadLine().ToCharArray();
+                char[] listChar;
+                //把重复的元素删掉
+                listChar = (chars.GroupBy(p => p).Select(p => p.Key).ToArray());
+                //遍历数组
+                for (int i = 0; i < listChar.Length; i++)
+                {
+                    //System.Diagnostics.Debug.WriteLine(listChar[i]);
+                    //System.Diagnostics.Debug.WriteLine(listChar.ToArray().Length);
+                    //如果没有这个元素的文件就创建
+                    if (listChar[i] != '/' && listChar[i] != ':' && listChar[i] != '.' && listChar[i] != ' ' && listChar[i] != '?')
+                    {
+                        StreamWriter file1 = new(file1Path + listChar[i], true, Encoding.UTF8);
+                        file1.WriteLine(n);
+                        file1.Close();
+                        file1.Dispose();
+                    }
+                }
+            }
+            file.Close();
+            file.Dispose();
+            System.Diagnostics.Debug.WriteLine("结束");
+        }
+
+        private void 书名框_TextChanged(object sender, EventArgs e)
+        {
+            if (!检索书名.IsBusy)
+            {
+                检索书名.DoWork += new DoWorkEventHandler(检索书名_DoWork);
+                检索书名.RunWorkerAsync(this);
+            }
+            if (书名框.Text == "")
+            {
+                索引框.Text = "";
+            }
+        }
+
+        private void 作者框_TextChanged(object sender, EventArgs e)
+        {
+            检索作者.DoWork += new DoWorkEventHandler(检索作者_DoWork);
+            检索作者.RunWorkerAsync(this);
+        }
+
+        private void 检索书名_DoWork(object sender, DoWorkEventArgs e)
+        {
+            char[] chars = 书名框.Text.ToCharArray();
+            char[] listChar;
+            List<int> list = new();
+            //把重复的元素删掉
+            listChar = (chars.GroupBy(p => p).Select(p => p.Key).ToArray());
+            try
+            {
+                for (int i = 0; i < listChar.Length; i++)
+                {
+                    if (listChar[i] != '/' && listChar[i] != ':' && listChar[i] != '.' && listChar[i] != ' ' && listChar[i] != '?')
+                    {
+                        StreamReader file2 = new(".\\数据索引\\" + listChar[i], Encoding.UTF8, true);
+                        // 判断是否读完
+                        while (!file2.EndOfStream)
+                        {
+                            int result = Convert.ToInt32(file2.ReadLine());
+                            list.Add(result);
+                        }
+                        int[] i2 = list.ToArray();
+                        i1 = i2.GroupBy(p => p).Select(p => p.Key).ToArray();
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                this.BeginInvoke(new Action(() => 结果.Text = ""));
+                return;
+                throw;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return;
+                throw;
+            }
+
+            this.BeginInvoke(new Action(() => 索引框.Text += i1.Length + Environment.NewLine));
+
+        }
+
+        private void 检索作者_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+        public void Out(CancellationToken sous)
+        {
+            StringBuilder sb = new();
+            StreamReader reader = new(file0);
+            System.Diagnostics.Debug.WriteLine("@");
+            this.BeginInvoke(new Action(() => 进度条.Maximum = i1.Length));
+            try
+            {
+                int k = 0;
+                for (int j = 0; !reader.EndOfStream; j++)
+                {
+                    if (j == i1[k])
+                    {
+                        //System.Diagnostics.Debug.WriteLine(reader.ReadLine());
+                        this.Invoke(new Action(() => 结果.Text += (k+1) + ":::" + reader.ReadLine() + Environment.NewLine));
+                        k++;
+                        this.BeginInvoke(new Action(() => 进度条.Value = k));
+                        if (k == i1.Length || sous.IsCancellationRequested)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        reader.ReadLine();
+                    }
+                }
+                reader.Close();
+                reader.Dispose();
+            }
+            catch (NullReferenceException)
+            {
+                throw;
+            }
+        }
+
+        private void 输出_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void 取消_Click(object sender, EventArgs e)
+        {
+            if (搜索_stop != null)
+            {
+                搜索_stop.Cancel();
+                进度条.Value = 0;
+                Array.Clear(i1);
+            }
+        }
+    }
+}
+
